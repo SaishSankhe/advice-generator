6c0794c700dae6850693ff787c7b16ff1a68a28c
Saish Sankhe <saish@saishs-laptop.mynetworksettings.com>
2023-02-26 13:31:35 -0500

    adds vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/main.scss';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
